160d5636e5c4e1c5f8210eae43fcfee04e481e7e
SanketBhat <sanket.r.bhat.u@gmail.com>
2018-09-26 13:33:53 +0530

    Initial commit
    - Refactored the starter code
    - Starter code - https://github.com/udacity/xyz-reader-starter-code.git

---
+++ XYZReader/src/main/java/com/example/xyzreader/adapter/Adapter.java
@@ -0,0 +1,127 @@
+package com.example.xyzreader.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.xyzreader.R;
+import com.example.xyzreader.data.ArticleLoader;
+import com.example.xyzreader.ui.DynamicHeightNetworkImageView;
+import com.example.xyzreader.ui.ImageLoaderHelper;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    private static final String TAG = "Adapter";
+    private OnItemClickListener clickListener;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss", Locale.getDefault());
+    // Use default locale format
+    @SuppressLint("SimpleDateFormat")
+    private SimpleDateFormat outputFormat = new SimpleDateFormat();
+    // Most time functions can only handle 1902 - 2037
+    private GregorianCalendar START_OF_EPOCH = new GregorianCalendar(2, 1, 1);
+    private Context mContext;
+    private Cursor mCursor;
+
+    public Adapter(Context context, Cursor cursor, OnItemClickListener listener) {
+        clickListener = listener;
+        mCursor = cursor;
+        mContext = context;
+    }
+
+    public void setCursor(Cursor mCursor) {
+        this.mCursor = mCursor;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        mCursor.moveToPosition(position);
+        return mCursor.getLong(ArticleLoader.Query._ID);
+    }
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_article, parent, false);
+        return new ViewHolder(view);
+    }
+
+    private Date parsePublishedDate() {
+        try {
+            String date = mCursor.getString(ArticleLoader.Query.PUBLISHED_DATE);
+            return dateFormat.parse(date);
+        } catch (ParseException ex) {
+            Log.e(TAG, ex.getMessage());
+            Log.i(TAG, "passing today's date");
+            return new Date();
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+        mCursor.moveToPosition(position);
+        holder.titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
+        Date publishedDate = parsePublishedDate();
+        if (!publishedDate.before(START_OF_EPOCH.getTime())) {
+
+            holder.subtitleView.setText(Html.fromHtml(
+                    DateUtils.getRelativeTimeSpanString(
+                            publishedDate.getTime(),
+                            System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                            DateUtils.FORMAT_ABBREV_ALL).toString()
+                            + "<br/>" + " by "
+                            + mCursor.getString(ArticleLoader.Query.AUTHOR)));
+        } else {
+            holder.subtitleView.setText(Html.fromHtml(
+                    outputFormat.format(publishedDate)
+                            + "<br/>" + " by "
+                            + mCursor.getString(ArticleLoader.Query.AUTHOR)));
+        }
+        holder.thumbnailView.setImageUrl(
+                mCursor.getString(ArticleLoader.Query.THUMB_URL),
+                ImageLoaderHelper.getInstance(mContext).getImageLoader());
+        holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor.getCount();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(long itemId);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        DynamicHeightNetworkImageView thumbnailView;
+        TextView titleView;
+        TextView subtitleView;
+
+        ViewHolder(View view) {
+            super(view);
+            thumbnailView = view.findViewById(R.id.thumbnail);
+            titleView = view.findViewById(R.id.article_title);
+            subtitleView = view.findViewById(R.id.article_subtitle);
+
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (clickListener != null) clickListener.onItemClick(getItemId());
+        }
+    }
+}
